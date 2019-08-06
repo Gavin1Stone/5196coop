5df7ffa369b6aa0ff1ac07fbb8967e930348bf2e
Zhicong Hu <zhuu0014@student.monash.edu>
2019-08-07 01:14:16 +1000

    Data cleaning is finished Waiting for the text of Patent kind

---
+++ Assignment1/outPutCsv2.py
@@ -0,0 +1,97 @@
+# -*- coding : utf-8 -*-
+# @time   : 2019-08-06 22:39
+# @Author : Zhicong Hu
+# @Studid : 29489636
+
+import re
+
+grant_id=[]
+patent_title=[]
+kind=[]
+number_of_claims=[]
+inventors=[]
+citations_applicant_count=[]
+citations_examnier_count=[]
+claim_text=[]
+abstract=[]
+
+inFile=open("sample_input.txt","r")
+para=re.findall("(?:<us-patent-grant.*?)(?:file=\")(.*?)(?:</us-patent-grant)",inFile.read(),re.S)
+for i in para:
+    #retrive patentid
+    tmpGrantId = re.findall("(.*?)(?:-\w+.XML.*?)", i, re.M)
+    grant_id.append(tmpGrantId[0])
+
+
+    #retrieve patent_title
+    tmpPatentTitle=re.findall('(?:<invention-title id=.*?>)(.*?)(?:</invention-title>)',i,re.S)
+    patent_title.append(tmpPatentTitle[0])
+
+
+    #retrieve kind
+    tmpKind=re.findall('(?:<doc-number>{0}.*?)(?:</doc-number>.*?)(?:<kind>)(.*?)(?:</kind>)'.format(tmpGrantId[0][2:]),i,re.S)
+    kind.append(tmpKind[0])
+
+
+    #retrieve number of claims
+
+    tmpNumOfClaim=re.findall('(?:<number-of-claims>)(.*?)(?:</number-of-claims>)',i,re.S)
+    number_of_claims.append(tmpNumOfClaim[0])
+
+
+
+    #retrieve inventors
+    tmpInventorsLname=re.findall("(?:<inventor)(?:.*?)(?:<last-name>)(.*?)(?:</last-name>)(?:.*?)(?:</inventor>)",i,re.S)
+    tmpInventorsFname = re.findall("(?:<inventor)(?:.*?)(?:<first-name>)(.*?)(?:</first-name>)(?:.*?)(?:</inventor>)",i,re.S)
+    tmpInventorList=''
+    for j in range(len(tmpInventorsLname)):
+        if j+1==len(tmpInventorsLname):
+            nameStr=tmpInventorsFname[j]+' '+tmpInventorsLname[j]
+        else:
+            nameStr = tmpInventorsFname[j] + ' ' + tmpInventorsLname[j]+","
+        tmpInventorList=tmpInventorList+nameStr
+    inventors.append(tmpInventorList)
+
+    #retrive citations applicant count and examiner count
+    tmpApplicantCitation=re.findall("(cited by applicant)",i)
+    tmpExaminerCitation=re.findall("(cited by examiner)",i)
+    citations_applicant_count.append(len(tmpApplicantCitation))
+    citations_examnier_count.append(len(tmpExaminerCitation))
+
+
+    #retrieve claim text
+    tmpClaimText1=re.findall("(?:<claim-text>)(.*?)(?:</claim-text>)",i,re.S)
+    for l in range(len(tmpClaimText1)):
+        tmpClaimText1[l]=re.sub('<claim-text>',' ',tmpClaimText1[l])
+        tmpClaimText1[l] = re.sub('<.*?claim-ref.*?>', ' ', tmpClaimText1[l])
+        tmpClaimText1[l] = re.sub('\n', '', tmpClaimText1[l])
+        tmpClaimText1[l] = re.sub('  ', ' ', tmpClaimText1[l])
+    ClaimStr=''
+    for m in tmpClaimText1:
+        ClaimStr+=m
+    claim_text.append(ClaimStr)
+
+    #finally retrieve abstract text
+    tmpAbstract=re.findall("(?:<abstract.*?>\n<p .*?>)(.*?)(?:</abstract>)",i,re.S)
+    for n in range(len(tmpAbstract)):
+        tmpAbstract[n]=re.sub("</p>",'',tmpAbstract[n])
+        tmpAbstract[n] = re.sub("\n", '', tmpAbstract[n])
+        tmpAbstract[n] = re.sub("  ", ' ', tmpAbstract[n])
+    AbsStr=''
+    for n in tmpAbstract:
+        AbsStr+=n
+    abstract.append(AbsStr)
+
+print(len(grant_id))
+print(len(patent_title))
+print(len(kind))
+print(len(number_of_claims))
+print(len(inventors))
+print(len(citations_applicant_count))
+print(len(citations_examnier_count))
+print(len(claim_text))
+print(len(abstract))
+
+
+inFile.close()
+print()
---
+++ Assignment1/outputCsv.py
@@ -0,0 +1,35 @@
+# -*- coding : utf-8 -*-
+# @time   : 2019-08-06 12:46
+# @Author : Zhicong Hu
+# @Studid : 29489636
+
+import re
+
+
+
+#grab the unique patent id
+inFile=open("sample_input.txt","r")
+grant_id=re.findall("(?:<us-patent-grant.*?file=\")(.*?)(?:-.*?)",inFile.read(),re.M)
+inFile.close()
+
+patentTitle=[]
+
+
+#grab the content of each paragraph by patent id
+inFile=open("sample_input.txt","r")
+
+for i in grant_id:
+    inFile = open("sample_input.txt", "r")
+    print(i)
+    para=re.findall("(?:<us-patent-grant.*?)(?:file=\"{0})(.*?)(?:</us-patent-grant)".format(i),inFile.read(),re.S)
+
+    #retrieve patent_title
+    tmpPatentTitle=re.findall('(?:<invention-title id=.*?>)(.*?)(?:</invention-title>)',para[0])
+    print((tmpPatentTitle))
+    patentTitle.append(tmpPatentTitle)
+
+
+    #retrieve kind
+
+    inFile.close()
+
